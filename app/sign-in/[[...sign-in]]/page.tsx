b52059c0a187acc584e1056c478c2694c67b703f
ChuanQiao1128 <qiaochua@myvuw.ac.nz>
2026-05-21 09:29:13 +1200

    Use redirect OAuth for Clerk Google signup

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -5,6 +5,10 @@ export default function SignInPage() {
     <main className="flex min-h-screen items-center justify-center bg-paper px-6">
       <SignUp
         forceRedirectUrl="/app"
+        fallbackRedirectUrl="/app"
+        oauthFlow="redirect"
+        path="/sign-in"
+        routing="path"
         signInUrl="/sign-in"
       />
     </main>
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -5,6 +5,10 @@ export default function SignUpPage() {
     <main className="flex min-h-screen items-center justify-center bg-paper px-6">
       <SignUp
         forceRedirectUrl="/app"
+        fallbackRedirectUrl="/app"
+        oauthFlow="redirect"
+        path="/sign-up"
+        routing="path"
         signInUrl="/sign-in"
       />
     </main>
